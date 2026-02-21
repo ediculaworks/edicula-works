6cf43b02f7707ee70bc6176bcb906470b5f0383b
ediculaworks <ediculaworks@gmail.com>
2026-02-21 12:35:34 -0300

    fix: remove load_dotenv, use env vars from docker

--- api/database.py
+++ api/database.py
@@ -1,8 +1,5 @@
 from supabase import create_client, Client
 import os
-from dotenv import load_dotenv
-
-load_dotenv()
 
 SUPABASE_URL = os.getenv("SUPABASE_URL")
 SUPABASE_SERVICE_KEY = os.getenv("SUPABASE_SERVICE_KEY")
